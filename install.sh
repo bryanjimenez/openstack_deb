770aa8f2e9c213d170ccbb0d38be941fef5dfbea
root <root@debian>
2013-10-03 11:25:20 -0400

    +install

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo "deb http://archive.gplhost.com/debian openstack main
+deb http://archive.gplhost.com/debian wheezy-backports main" >> /etc/apt/sources.list
+
+wget -qO - http://archive.gplhost.com/debian/repository_key.asc | apt-key add -
+
+apt-get update
+
+wget "http://anonscm.debian.org/gitweb/?p=openstack/openstack-auto-builder.git;a=blob_plain;f=preseeder" -O preseeder
+
+chmod +x preseeder
+
+sed -i "s/changeme/ADMIN/" ./preseeder
+
+./preseeder
+
+apt-get install openstack-toaster
+
+echo "export SERVICE_ENDPOINT=http://127.0.0.1:35357/v2.0/
+export SERVICE_TOKEN=ADMIN
+
+export OS_USERNAME=admin
+export OS_PASSWORD=ADMIN
+export OS_TENANT_NAME=admin
+export OS_AUTH_URL=http://127.0.0.1:5000/v2.0/
+export OS_VERSION=1.1
+export OS_NO_CACHE=1
+" > stackrc
